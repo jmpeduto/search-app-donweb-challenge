3a0f9b1699fc6ad89a3d4913ec94ff635733b149
Juan Peduto <jmpeduto@gmail.com>
2021-12-15 12:17:01 -0300

    hasta desplegar carrito

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,27 +3,41 @@ import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 import { SearchServiceService } from './services/search-service.service';
 import { Categoria } from './interfaces/categoria.interface';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'search-app';
-  public searchText:any;
-  listado:Categoria[] = [];
+  public searchText: any;
+  listado: Categoria[] = [];
+  showCart: boolean = false;
 
-  public dataset:any[] = ['MDB', 'Angular', 'Bootstrap', 'Framework', 'SPA', 'React', 'Vue'];
+  public dataset: any[] = [
+    'MDB',
+    'Angular',
+    'Bootstrap',
+    'Framework',
+    'SPA',
+    'React',
+    'Vue',
+  ];
 
-  constructor(private searchService:SearchServiceService){}
+  constructor(private searchService: SearchServiceService) {}
 
-  public search(text:any){
+  public search(text: any) {
     console.log(text);
     console.log(this.listado);
   }
 
-  ngOnInit(){
-    this.searchService.getAllCategorias().subscribe((resp: any) => (this.listado = resp.data));;
+  ngOnInit() {
+    this.searchService
+      .getAllCategorias()
+      .subscribe((resp: any) => (this.listado = resp.data));
+  }
+
+  toggle() {
+    this.showCart = !this.showCart;
   }
 }
