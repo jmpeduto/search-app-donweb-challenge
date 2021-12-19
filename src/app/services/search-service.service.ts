5d68d47b39c06ff5fe650ee3ce2e3f37422afb9e
Juan Peduto <jmpeduto@gmail.com>
2021-12-19 14:12:21 -0300

    staged

--- src/app/services/search-service.service.ts
+++ src/app/services/search-service.service.ts
@@ -4,28 +4,36 @@ import { map } from 'rxjs/operators';
 import { BehaviorSubject } from 'rxjs';
 import { donwebApiRoutes } from '../api.donweb.routes';
 import { Plan } from '../interfaces/plan';
+import * as express from 'express';
+import { createProxyMiddleware, Filter, Options, RequestHandler } from 'http-proxy-middleware';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class SearchServiceService {
-
+  
   private httpOptions = {
     headers: new HttpHeaders({ 
       'Access-Control-Allow-Origin':'*',
       'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
     })
   };
- 
+
+  
   public listado: Plan[] = [];
   private listadoSource: any = new BehaviorSubject(this.listado);
   currentListado$ = this.listadoSource.asObservable();
   cargadoListado: boolean = false;
-
+  
   constructor(private http: HttpClient) {
     this.getListado();
+  //   let app = express();
+  
+  //   app.use('/getListado', createProxyMiddleware({ target: 'http://c1300044.ferozo.com/getListado.php', changeOrigin: true }));
+  //   app.listen(3000);
   }
-
+  
   llamadaPruebaCors(){
     const headers = { 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept' };
     const body = {  };
